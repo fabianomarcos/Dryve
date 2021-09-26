c9a3a9577bf9ca30021b5c3f139c7d299f640079
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-09-25 23:05:17 -0300

    feature: creation of graphiccomponent

---
+++ src/components/Graphic/styles.ts
@@ -0,0 +1,11 @@
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.div`
+    background: #ffffff;
+    border: 0.2px solid ${shade(0.1, '#ffffff')};
+    height: 264px;
+    min-width: 828px;
+    width: 100%;
+    border-radius: 8px;
+`;
