4104860482ef5eb9598f56cbed8ab5ecf7592b5f
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-09-25 23:06:58 -0300

    feature: creation of interface to cars

---
+++ src/interfaces/IDataCars.ts
@@ -0,0 +1,18 @@
+export interface IDataCars {
+    mileage: number;
+    model_year: number;
+    manufacturing_year: number;
+    ad_selling_price: number;
+    evaluation_type: string;
+    version_uuid: string;
+    name: string;
+    transmission_type: string;
+    fuel_type: string;
+    vehicle_uuid: string;
+    model_name: string;
+    version_name: string;
+    image: string;
+    brand_name: string;
+    model_uuid: string;
+    brand_uuid: string;
+}
