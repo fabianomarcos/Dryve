cf8cd0d24cb881def8790c149d893903d9d69e01
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-09-27 22:19:17 -0300

    refactor: identation

--- src/components/HeaderCard/index.tsx
+++ src/components/HeaderCard/index.tsx
@@ -1,23 +1,24 @@
+/* eslint-disable react/jsx-indent */
 import React from 'react';
 
 import { Container } from './styles';
 
 interface IProps {
-  icon: JSX.Element;
-  title: string;
-  data: string;
+    icon: JSX.Element;
+    title: string;
+    data: string;
 }
 
 const HeaderCard: React.FC<IProps> = ({ icon, title, data }) => {
-  return (
-    <Container>
-      {title}
-      <div>
-        {data}
-        {icon}
-      </div>
-    </Container>
-  );
+    return (
+        <Container>
+            {title}
+            <div>
+                {data}
+                {icon}
+            </div>
+        </Container>
+    );
 };
 
 export default HeaderCard;
