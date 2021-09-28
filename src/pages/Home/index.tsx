8abb6199744aeea6938ef502f32e2cdddf0d51a5
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-09-27 22:18:07 -0300

    feature: finalized test drive cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+
+import api from '../../services/api';
+
 import Graphic from '../../components/Graphic';
 import HeaderCard from '../../components/HeaderCard';
 import { generalIcons } from '../../components/icons/icons';
 import LastLeads from '../../components/LastLeads';
 import TestDriver from '../../components/TestDriver';
-import { IDataCars } from '../../interfaces/IDataCars';
-import api from '../../services/api';
+
+import { IDataCars, IStatusTestDriver } from '../../interfaces/types';
 
 import {
     Container,
@@ -22,11 +25,31 @@ interface IProps {
 
 const Home: React.FC<IProps> = ({ title }) => {
     const [cars, setCars] = useState<IDataCars[]>([]);
+    const expiredTestDrivers = useMemo(
+        () => cars.filter(c => c.expired),
+        [cars],
+    );
+    const validTestDrivers = useMemo(
+        () => cars.filter(c => !c.expired),
+        [cars],
+    );
 
     useEffect(() => {
         (async () => {
             const response = await api.get<IDataCars[]>('/cars');
-            setCars(response.data);
+
+            const statusTestDrive = await api.get<IStatusTestDriver[]>(
+                '/test-driver',
+            );
+
+            const carsFormatted = response.data.map(d => {
+                const car = statusTestDrive.data.find(
+                    status => status.vehicle_uuid === d.vehicle_uuid,
+                );
+                return { ...d, expired: car?.expired || false };
+            });
+
+            setCars(carsFormatted);
         })();
     }, []);
 
@@ -51,8 +74,14 @@ const Home: React.FC<IProps> = ({ title }) => {
             </ContainerGraphic>
 
             <ContentTestDriver>
-                <TestDriver title="Test-drives vencidos" cars={cars} />
-                <TestDriver title="Test-drives do dia" cars={cars} />
+                <TestDriver
+                    title="Test-drives vencidos"
+                    cars={expiredTestDrivers}
+                />
+                <TestDriver
+                    title="Test-drives do dia"
+                    cars={validTestDrivers}
+                />
             </ContentTestDriver>
         </Container>
     );
