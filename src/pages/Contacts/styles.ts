9fe32f8c26a91bc804d4da54fe01bbfb7c07c581
Fabiano Marcos <fabiano.marcos@seidor.com.br>
2021-09-30 00:16:48 -0300

    styles: centralized

--- src/pages/Contacts/styles.ts
+++ src/pages/Contacts/styles.ts
@@ -24,6 +24,12 @@ export const Container = styled.div`
         display: flex;
         gap: 15px;
         align-items: center;
+
+        span {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
     }
 `;
 
